4f09ee52268be7e19e6b4fa5fe6b3230c1a89737
Brian <parks.271@osu.edu>
2015-07-04 09:35:52 -0700

    quick fix to ignore album art and to check if music has id3v1 tag.

--- MP3Renamer/src/RenamerMain.java
+++ MP3Renamer/src/RenamerMain.java
@@ -10,7 +10,7 @@ import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
 public class RenamerMain {
-
+	
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 		
@@ -20,12 +20,12 @@ public class RenamerMain {
 		
 		Files.walk(Paths.get(path)).forEach(filePath -> {
 			if (Files.isRegularFile(filePath)){
-				System.out.println(filePath);
+				//System.out.println(filePath);
 				try {
 					Rename(filePath.toString());
 				} catch (Exception e) {
 					System.out.println(filePath + " failed to rename");
-					e.printStackTrace();
+					//e.printStackTrace();
 				}
 			}
 		}); 
@@ -34,17 +34,27 @@ public class RenamerMain {
 	}
 	private static void Rename(String filePath) throws UnsupportedTagException, InvalidDataException, IOException{
 		Mp3File mp3file = new Mp3File(filePath);
-	    ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+		 ID3v1 id3v1Tag;
+		if (mp3file.hasId3v1Tag()){
+			
+			 id3v1Tag = mp3file.getId3v1Tag();
+			 // TODO Sanitize incoming meta data
+			 String artist = id3v1Tag.getArtist();
+			 String title = id3v1Tag.getTitle();
+			    
+		    
+		    File oldFile = new File(filePath);
+		    String newName = artist + " - " + title +".mp3";
+		   
+		    File newFile = new File(oldFile.getParent(),newName);
+		    Files.move(oldFile.toPath(), newFile.toPath());
+		    System.out.println(filePath + " was renamed successfully");
+		}else{
+			System.out.println("error: no information obtained.");
+		}
+	   
 		
-	    String artist = id3v1Tag.getArtist();
-	    String title = id3v1Tag.getTitle();
-	    
-	    
-	    File oldFile = new File(filePath);
-	    String newName = artist + " - " + title +".mp3";
-	    
-	    File newFile = new File(oldFile.getParent(),newName);
-	    Files.move(oldFile.toPath(), newFile.toPath());
+	   
 	    
 	}
 }
